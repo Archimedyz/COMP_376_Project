1493d5280b9c2ff421052b47e26dfa6d8f2cbc6f
Awais Ali <awais1994@hotmail.com>
2015-11-01 15:29:45 -0500

    Renamed 'HealthPiece' to 'HealthFragment'. HealthBar now instatiates health fragments as children and loads all of them.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class HealthBar : MonoBehaviour {
 
+    [SerializeField]
+    GameObject mHealthFragmentPrefab;
+
+    private GameObject[] mHealth;
+
+    private const float healthDisplayOffset = -1.95f;
+    private const int maxHeathFragments = 79;
+
 	// Use this for initialization
 	void Start () {
-	
+        mHealth = new GameObject[maxHeathFragments];
+        for (int i = 0; i < maxHeathFragments; ++i)
+        {
+            mHealth[i] = Instantiate(mHealthFragmentPrefab, new Vector3(healthDisplayOffset + (i * 0.05f) + transform.position.x, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
+            mHealth[i].transform.parent = gameObject.transform;
+        }
 	}
 	
 	// Update is called once per frame

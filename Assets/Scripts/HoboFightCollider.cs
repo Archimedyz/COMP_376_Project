a46b4e88ba8d8d72b1cef1dfe43765dee55b8762
Lukas Cardot-Goyette <lukascgoyette@hotmail.com>
2015-11-01 16:36:09 -0500

    Player puch back

---
+++ Assets/Scripts/HoboFightCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoboFightCollider : MonoBehaviour
+{
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.name == "GeneralColliderPlayer") {
+			col.gameObject.transform.parent.GetComponent<Player> ().GetHit (gameObject.transform.parent.GetComponent<Hobo> ().GetFacingDirection ());
+		}
+	}
+}

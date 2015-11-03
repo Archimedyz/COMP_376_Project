396640edf17d2f3a3fdc2d06f66bb3b6e1a43b54
Awais Ali <awais1994@hotmail.com>
2015-11-02 23:16:29 -0500

    Added Parallax Functionality. Added ParallaxLayer Script.

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class ParallaxBackground : MonoBehaviour {
 
+    private ParallaxLayer[] mLayers;
+    private Vector3 mInitPosition;
+    private float oldX;
+
 	// Use this for initialization
 	void Start () {
-	
+        mLayers = GetComponentsInChildren<ParallaxLayer>();
+        for (int i = 0; i < mLayers.Length; ++i)
+        {
+            mLayers[i].SetParallaxFactor(0.10f - 0.025f * i);
+        }
+        mInitPosition = transform.position;
+        oldX = transform.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.position = new Vector3(transform.position.x, mInitPosition.y, mInitPosition.z);
+        if(transform.position.x != oldX) {
+            float deltaX = transform.position.x - oldX;
+            oldX = transform.position.x;
+            foreach(ParallaxLayer layer in mLayers) {
+                layer.Move(deltaX);
+            }
+        }
 	}
 }
---
+++ Assets/Scripts/ParallaxLayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxLayer : MonoBehaviour {
+
+    Vector3 mInitPosition;
+    public float parallaxFactor;
+	// Use this for initialization
+	void Start () {
+        mInitPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Move(float deltaX)
+    {
+        transform.Translate(new Vector3(-deltaX, 0.0f, 0.0f) * parallaxFactor);
+    }
+
+    public void SetParallaxFactor(float factor)
+    {
+        parallaxFactor = factor;
+    }
+}

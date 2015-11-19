c10f734df3b94e7ff3e8e9418851bcbf0bf8d664
Awais Ali <awais1994@hotmail.com>
2015-11-19 18:49:25 -0500

    Added metro png file. working on script for metr movement and collision.

---
+++ Assets/Scripts/Environment/Metro.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Metro : MonoBehaviour {
+
+    public float mSpeed;
+    public bool goingRight;
+
+    SpriteRenderer mSpriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+	    mSpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate((goingRight ? Vector3.right : Vector3.left) * mSpeed * Time.deltaTime);
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player") {
+            Debug.Log("Metro hits player.");
+        }
+    }
+
+    public void SetOrderInLayer(int orderInLayer) {
+        mSpriteRenderer.sortingOrder = orderInLayer;
+    }
+}

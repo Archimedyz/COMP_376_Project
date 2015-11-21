10dd816b4c6c93907c372a8828f40cc89d90c91b
Lukas Cardot-Goyette <lukascgoyette@hotmail.com>
2015-11-21 14:19:38 -0500

    Boss changes

--- Assets/Scripts/Enemies/DigDugBoss/Boss1Controller.cs
+++ Assets/Scripts/Enemies/DigDugBoss/Boss1Controller.cs
@@ -5,25 +5,25 @@ public class Boss1Controller : MonoBehaviour
 {
 	public GameObject fygarPrefab;
 	public GameObject pookaPrefab;
-
+	
 	private Player player;
 	private GameObject[] enemies;
 	public DigDug digdug;
-
+	
 	int bossDifficulty = 4;
-
+	
 	private bool canMove;
-
+	
 	public Vector3 playerPosition;
 	private float enemySpeed = 1.25f;
-
+	
 	private FloorController mFloorControllerRef;
 	public int mFloorIndex;
 	public float[] mFloorBoundary;
 	private SpriteRenderer mSpriteRenderer;
 	private int mInitialOrderInLayer;
 	private bool floorBoundaryInitialized;
-
+	
 	void Start ()
 	{
 		player = GameObject.Find ("Player").GetComponent<Player> ();
@@ -31,11 +31,11 @@ public class Boss1Controller : MonoBehaviour
 		enemies = new GameObject[6];
 		CreateWave ();
 	}
-
+	
 	void Update ()
 	{
 		if (enemies [0] != null) {
-			if (enemies [0].transform.position.y >= enemies [0].GetComponent<Pooka> ().GetInitialPosition ().y) {
+			if (enemies [2].transform.position.y >= enemies [2].GetComponent<Pooka> ().GetFloorBoundaryY ()) {
 				player.SetCanMove (true);
 				digdug.SetCanMove (true);
 				for (int i = 0; i < bossDifficulty; i++) {
@@ -48,33 +48,33 @@ public class Boss1Controller : MonoBehaviour
 			}
 		}
 	}
-
+	
 	public void CreateWave ()
 	{
 		enemySpeed += 0.25f;
 		player.SetCanMove (false);
 		player.SetPosition (playerPosition);
 		digdug.SetCanMove (false);
-
+		
 		if (enemies [0] != null) {
 			for (int i = 0; i < transform.childCount; i++) {
 				Destroy (enemies [i]);
 			}
 		}
-
+		
 		for (int i = 0; i < bossDifficulty; i++) {
 			if (i < 3) {
-				enemies [i] = Instantiate (pookaPrefab, new Vector3 (-3.1f + (i * 0.3f), -7f + (i * 2), -1f), Quaternion.identity) as GameObject;
+				enemies [i] = Instantiate (pookaPrefab, new Vector3 ((digdug.transform.position.x - 8f) + (i * 0.3f), -7f + (i * 2), -1f), Quaternion.identity) as GameObject;
 				enemies [i].transform.parent = transform;
 				enemies [i].GetComponent<Pooka> ().SetSpeed (enemySpeed);
 			} else {
-				enemies [i] = Instantiate (fygarPrefab, new Vector3 (-1.5f + ((i - 3) * 0.3f), -7f + ((i - 3) * 2), -1f), Quaternion.identity) as GameObject;
+				enemies [i] = Instantiate (fygarPrefab, new Vector3 ((digdug.transform.position.x - 5.5f) + ((i - 3) * 0.3f), -7f + ((i - 3) * 2), -1f), Quaternion.identity) as GameObject;
 				enemies [i].transform.parent = transform;
 				enemies [i].GetComponent<Fygar> ().SetSpeed (enemySpeed);
 			}
 		}
 	}
-
+	
 	public void IncreaseDifficulty ()
 	{
 		bossDifficulty ++;

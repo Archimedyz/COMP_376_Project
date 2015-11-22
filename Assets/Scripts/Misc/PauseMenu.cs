f27bc8c594c1eee5e4020370325d3b7040a4931c
Awais Ali <awais1994@hotmail.com>
2015-11-22 16:21:37 -0500

    Added new tag. implementing pause menu script

--- Assets/Scripts/Misc/PauseMenu.cs
+++ Assets/Scripts/Misc/PauseMenu.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class PauseMenu : MonoBehaviour {
 
+    GameObject mPausePanel;
+    private bool paused;
+
 	// Use this for initialization
 	void Start () {
-	
+        mPausePanel = GetComponentInChildren<RectTransform>().gameObject;
+        paused = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(paused) {
+            if(Input.GetButtonDown("Pause")) {
+                Debug.Log("Resuming.");
+                mPausePanel.SetActive(false);
+                Time.timeScale = 1;
+                paused = false;
+            }
+        } else {
+            if (Input.GetButtonDown("Pause")) {
+                Debug.Log("Pausing.");
+                mPausePanel.SetActive(true);
+                Time.timeScale = 0;
+                paused = true;
+            }
+        }
 	}
 }

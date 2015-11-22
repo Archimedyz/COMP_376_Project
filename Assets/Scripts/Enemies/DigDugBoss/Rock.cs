b1e5ac525a736343a4cf2d5d74ed63e26e028179
Lukas Cardot-Goyette <lukascgoyette@hotmail.com>
2015-11-22 16:20:39 -0500

    Rocks

--- Assets/Scripts/Enemies/DigDugBoss/Rock.cs
+++ Assets/Scripts/Enemies/DigDugBoss/Rock.cs
@@ -4,38 +4,52 @@ using System.Collections;
 public class Rock : MonoBehaviour
 {
 
-    GameObject mShadow;
+	GameObject mShadow;
 
-    void Start()
-    {
-        mShadow = transform.parent.GetChild(1).gameObject;
-    }
+	private AudioSource falling;
+	private AudioSource hit;
 
-    void Update()
-    {
-        if(transform.localPosition.y <= 0.0f) {
-            Destroy(transform.parent.gameObject);
-            return;
-        }
-        float shadowScale = Mathf.Min(1.5f/transform.localPosition.y, 1.0f);
-        mShadow.transform.localScale = new Vector3(shadowScale, shadowScale, mShadow.transform.localScale.z);
-    }
+	void Start ()
+	{
+		mShadow = transform.parent.GetChild (1).gameObject;
+
+		AudioSource[] audioSources = GetComponents<AudioSource> ();
+		falling = audioSources [0];
+		falling.Play ();
+		hit = audioSources [1];
+	}
+
+	void Update ()
+	{
+		if (transform.localPosition.y <= 0.0f) {
+			falling.Stop ();
+			hit.Play ();
+			Destroy (transform.parent.gameObject);
+			return;
+		}
+		float shadowScale = Mathf.Min (1.5f / transform.localPosition.y, 1.0f);
+		mShadow.transform.localScale = new Vector3 (shadowScale, shadowScale, mShadow.transform.localScale.z);
+	}
 
 	void OnTriggerEnter (Collider col)
 	{
+		hit.Play ();
 		if (col.gameObject.name == "GeneralColliderPlayer") {
-            // determine the Players Script.
-            Player player = col.gameObject.transform.parent.GetComponent<Player>();
-            if(Mathf.Abs(player.GetFootY() - (transform.parent.position.y - 0.3f)) <= 0.2f) {
-                player.GetHit(Vector2.left, 10);
-                Destroy(transform.parent.gameObject);
-            }
+			// determine the Players Script.
+			Player player = col.gameObject.transform.parent.GetComponent<Player> ();
+			if (Mathf.Abs (player.GetFootY () - (transform.parent.position.y - 0.3f)) <= 0.2f) {
+				falling.Stop ();
+				player.GetHit (Vector2.left, 10);
+				Destroy (transform.parent.gameObject);
+			}
 		} else if (col.gameObject.name == "Pooka") {
-            col.gameObject.GetComponent<Pooka>().SetLife(0);
-            Destroy(transform.parent.gameObject);
+			falling.Stop ();
+			col.gameObject.GetComponent<Pooka> ().SetLife (0);
+			Destroy (transform.parent.gameObject);
 		} else if (col.gameObject.name == "Fygar") {
+			falling.Stop ();
 			col.gameObject.GetComponent<Fygar> ().SetLife (0);
-            Destroy(transform.parent.gameObject);
+			Destroy (transform.parent.gameObject);
 		}
 	}
 }

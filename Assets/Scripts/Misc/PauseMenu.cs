2f001de32d948daa91573f12b24a51a43fcad573
Awais Ali <awais1994@hotmail.com>
2015-11-22 16:49:07 -0500

    Finished Bacis layout and selecting of button. Moving on to actually implementing the button actions.

--- Assets/Scripts/Misc/PauseMenu.cs
+++ Assets/Scripts/Misc/PauseMenu.cs
@@ -4,11 +4,21 @@ using System.Collections;
 public class PauseMenu : MonoBehaviour {
 
     GameObject mPausePanel;
+    UnityEngine.UI.Button[] menuButtons;
     private bool paused;
+    private int buttonSelected;
 
 	// Use this for initialization
 	void Start () {
-        mPausePanel = GetComponentInChildren<RectTransform>().gameObject;
+        mPausePanel = GameObject.FindGameObjectWithTag("PauseMenu");
+        menuButtons = mPausePanel.GetComponentsInChildren<UnityEngine.UI.Button>();
+        Debug.Log("menuButton.Length: " + menuButtons.Length);
+        for(int i = 0; i < menuButtons.Length; ++i) {
+            Debug.Log(menuButtons[i].gameObject.name);
+        }
+
+        // do this at the end of start, so we get everything in the panel
+        mPausePanel.SetActive(false);
         paused = false;
 	}
 	
@@ -16,15 +26,21 @@ public class PauseMenu : MonoBehaviour {
 	void Update () {
 	    if(paused) {
             if(Input.GetButtonDown("Pause")) {
-                Debug.Log("Resuming.");
                 mPausePanel.SetActive(false);
                 Time.timeScale = 1;
                 paused = false;
+            } else if(Input.GetButtonDown("Up")) {
+                buttonSelected = (buttonSelected - 1) % menuButtons.Length;
+                menuButtons[buttonSelected].Select();
+            } else if(Input.GetButtonDown("Down")) {
+                buttonSelected = (buttonSelected + 1) % menuButtons.Length;
+                menuButtons[buttonSelected].Select();
             }
         } else {
             if (Input.GetButtonDown("Pause")) {
-                Debug.Log("Pausing.");
                 mPausePanel.SetActive(true);
+                buttonSelected = 0;
+                menuButtons[buttonSelected].Select();
                 Time.timeScale = 0;
                 paused = true;
             }

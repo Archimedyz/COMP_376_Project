044d8d1fd18e1c6cd81df1cb984593b672cda554
Awais Ali <awais1994@hotmail.com>
2015-11-22 18:52:01 -0500

    Minor overhaul of the MainMenu Script.

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -4,20 +4,57 @@ using System.Collections;
 public class MainMenu : MonoBehaviour
 {
 
-    GameObject mMenuPanel;
-    UnityEngine.UI.Button[] menuButtons;
+    private int activeMenu;
+
+    GameObject[] mMenuPanels;
+    UnityEngine.UI.Button[][] mainMenuButtons;
     private int buttonSelected;
+
+    private int mainMenuIndex = -1;
+    private int newMenuIndex = -1;
+    private int loadMenuIndex = -1;
+
 
     // Use this for initialization
     void Start()
     {
-        mMenuPanel = GameObject.FindGameObjectWithTag("MainMenu");
-        menuButtons = mMenuPanel.GetComponentsInChildren<UnityEngine.UI.Button>();
+        mMenuPanels = GameObject.FindGameObjectsWithTag("MenuPanel");
+        mainMenuButtons = new UnityEngine.UI.Button[mMenuPanels.Length][];
+
+        Debug.Log("Length: " + mMenuPanels.Length);
+
+        for (int i = 0; i < mMenuPanels.Length; ++i)
+        {
+            mainMenuButtons[i] = mMenuPanels[i].GetComponentsInChildren<UnityEngine.UI.Button>();
+            Debug.Log(mMenuPanels[i].name + " - Buttons[" + i + "]: " + mainMenuButtons[i].Length);
+            mainMenuButtons[i][0].Select();
+
+            if(mMenuPanels[i].name == "MainPanel") {
+                mainMenuIndex = i;
+            } else if(mMenuPanels[i].name == "NewPanel") {
+                newMenuIndex = i;
+            } else if(mMenuPanels[i].name == "LoadPanel") {
+                loadMenuIndex = i;
+            }
+
+            if(i != mainMenuIndex) {
+                mMenuPanels[i].SetActive(false);
+            }
+            for (int j = 0; j < mainMenuButtons[i].Length; ++j)
+            {
+                Debug.Log(mainMenuButtons[i][j].gameObject.name);
+            }
+        }
+
+        activeMenu = mainMenuIndex;
         buttonSelected = 0;
-        menuButtons[buttonSelected].Select();
 
         // check if there exists a save file. If yes, allow for continue.
-        // TODO
+        int gamesSaved = PlayerPrefs.GetInt("games_saved");
+        if(gamesSaved == 0) {
+            Destroy(mainMenuButtons[mainMenuIndex][0].gameObject);
+            mainMenuButtons[mainMenuIndex][0].Select();            
+        }
     }
 
     // Update is called once per frame
@@ -25,13 +62,13 @@ public class MainMenu : MonoBehaviour
     {        
         if (Input.GetButtonDown("Up"))
         {
-            buttonSelected = (buttonSelected + menuButtons.Length - 1) % menuButtons.Length; // add menuButtons.Length because % allows for negative remainders . . .
-            menuButtons[buttonSelected].Select();
+            buttonSelected = (buttonSelected + mainMenuButtons[activeMenu].Length - 1) % mainMenuButtons[activeMenu].Length; // add menuButtons.Length because % allows for negative remainders . . .
+            mainMenuButtons[activeMenu][buttonSelected].Select();
         }
         else if (Input.GetButtonDown("Down"))
         {
-            buttonSelected = (buttonSelected + 1) % menuButtons.Length;
-            menuButtons[buttonSelected].Select();
+            buttonSelected = (buttonSelected + 1) % mainMenuButtons[activeMenu].Length;
+            mainMenuButtons[activeMenu][buttonSelected].Select();
         }
     }
 
@@ -43,13 +80,13 @@ public class MainMenu : MonoBehaviour
 
     void NewGame()
     {
-        buttonSelected = 1;
+        buttonSelected = 0;
         Debug.Log("New Game");
+
     }
 
     void Quit()
     {
-        buttonSelected = 2;
         Application.Quit();
         Debug.Log("Quit");
         Debug.DebugBreak();

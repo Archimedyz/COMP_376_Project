396151e26cf0107c3238ae22d3af54936f1705e2
Lukas Cardot-Goyette <lukascgoyette@hotmail.com>
2015-11-23 17:50:26 -0500

    Started dialogue

---
+++ Assets/Scripts/Dialogue/DialogueTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogueTest : MonoBehaviour
+{
+
+	private Text thisText;
+	
+	void Start ()
+	{
+		thisText = gameObject.GetComponent<Text> ();
+	}
+	
+	void Update ()
+	{
+		thisText.text = "Allo";
+	}
+}

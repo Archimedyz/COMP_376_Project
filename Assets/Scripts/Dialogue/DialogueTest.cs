cdda618044907ad3642e61b27a71ae063b0a836d
Lukas Cardot-Goyette <lukascgoyette@hotmail.com>
2015-11-23 18:28:24 -0500

    Cool dialogue :D

--- Assets/Scripts/Dialogue/DialogueTest.cs
+++ Assets/Scripts/Dialogue/DialogueTest.cs
@@ -1,19 +1,45 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.IO;
 
 public class DialogueTest : MonoBehaviour
 {
 
 	private Text thisText;
 	
+	protected FileInfo theSourceFile = null;
+	protected StreamReader reader = null;
+	protected string text = " ";
+	
 	void Start ()
 	{
 		thisText = gameObject.GetComponent<Text> ();
+		theSourceFile = new FileInfo ("Assets/Scripts/Dialogue/dialogueTest.txt");
+		reader = theSourceFile.OpenText ();
 	}
 	
 	void Update ()
 	{
-		thisText.text = "Allo";
+		if (text != null) {
+			if (Input.anyKeyDown) {
+				StartCoroutine (DisplayText ());
+			}
+		}
+	}
+
+	private IEnumerator DisplayText ()
+	{
+		int charLineNumber = 0;
+		text = reader.ReadLine ();
+		thisText.text = "";
+		for (int i = 0; i < text.Length; i++, charLineNumber++) {
+			if (text [i] == '$') {
+				thisText.text += "\n";
+			} else {
+				thisText.text += text [i];
+			}
+			yield return new WaitForSeconds (0.05f);
+		}
 	}
 }

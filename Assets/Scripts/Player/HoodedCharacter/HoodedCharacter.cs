5c1e18b67941ada3e7d052dfbf21df64e313e396
Lukas Cardot-Goyette <lukascgoyette@hotmail.com>
2015-11-24 21:43:26 -0500

    Hooded Character

---
+++ Assets/Scripts/Player/HoodedCharacter/HoodedCharacter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoodedCharacter : MonoBehaviour
+{
+	private bool mMoving;
+	private bool mWaving;
+	private bool mDissapear = false;
+
+	private Animator mAnimator;
+
+	void Start ()
+	{
+		mAnimator = GetComponent<Animator> ();
+	}
+	
+
+	void Update ()
+	{
+		ResetBoolean ();
+	}
+
+	private void ResetBoolean ()
+	{
+		mMoving = false;
+		mWaving = false;
+	}
+
+	private void UpdateAnimator ()
+	{
+		mAnimator.SetBool ("isWaving", mWaving);
+		mAnimator.SetBool ("isMoving", mMoving);
+		mAnimator.SetBool ("isDisappearing", mDissapear);
+	}
+}

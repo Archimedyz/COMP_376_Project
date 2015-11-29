27d4bfd167dbaa91a65618424ace68dbc1c50837
Lukas Cardot-Goyette <lukascgoyette@hotmail.com>
2015-11-29 17:53:25 -0500

    Fixed some stuff

--- Assets/Scripts/Control/LevelStart.cs
+++ Assets/Scripts/Control/LevelStart.cs
@@ -1,23 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LevelStart : MonoBehaviour {
+public class LevelStart : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
-        if (GameObject.FindGameObjectWithTag("Player") != null)
-        {
-            GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
-            Destroy(gameObject);
-        }
+	void Start ()
+	{
+		if (GameObject.FindGameObjectWithTag ("Player") != null) {
+			// GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
+			Destroy (gameObject);
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (GameObject.FindGameObjectWithTag("Player") != null)
-        {
-            GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
-            Destroy(gameObject);
-        }
+	void Update ()
+	{
+		if (GameObject.FindGameObjectWithTag ("Player") != null) {
+			GameObject.FindGameObjectWithTag ("Player").transform.position = transform.position;
+			Destroy (gameObject);
+		}
 	}
 }
--- Assets/Scripts/Enemies/Hobo/HoboFightCollider.cs
+++ Assets/Scripts/Enemies/Hobo/HoboFightCollider.cs
@@ -3,18 +3,14 @@ using System.Collections;
 
 public class HoboFightCollider : MonoBehaviour
 {
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.gameObject.name == "GeneralColliderPlayer")
-        {
-            if (col.gameObject.transform.parent.GetComponent<Player>().IsDefending())
-            {
-                gameObject.transform.parent.GetComponent<Hobo>().Recoil(col.gameObject.transform.parent.GetComponent<Player>().GetFacingDirection(), 3.0f);
-            }
-            else
-            {
-                col.gameObject.transform.parent.GetComponent<Player>().GetHit(gameObject.transform.parent.GetComponent<Hobo>().GetFacingDirection(), gameObject.transform.parent.GetComponent<Hobo>().mStats.DoDynamicDamage());
-            }
-        }
-    }
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.name == "GeneralColliderPlayer") {
+			if (col.gameObject.transform.parent.GetComponent<Player> ().IsDefending ()) {
+				gameObject.transform.parent.GetComponent<Hobo> ().Recoil (col.gameObject.transform.parent.GetComponent<Player> ().GetFacingDirection (), 3.0f);
+			} else {
+				col.gameObject.transform.parent.GetComponent<Player> ().GetHit (gameObject.transform.parent.GetComponent<Hobo> ().GetFacingDirection (), gameObject.transform.parent.GetComponent<Hobo> ().mStats.DoDynamicDamage ());
+			}
+		}
+	}
 }

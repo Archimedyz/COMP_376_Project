61083f6bbc3676746bf56fda2ad8a23e37432cf0
Lukas Cardot-Goyette <lukascgoyette@hotmail.com>
2015-11-29 11:25:51 -0500

    Enemy spawner dying

--- Assets/Scripts/Enemies/Spawner.cs
+++ Assets/Scripts/Enemies/Spawner.cs
@@ -3,12 +3,105 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour
 {
+	private bool mGetHit;
+	private bool mDying;
+	private float dyingTimer = 0.0f;
+	
+	private Transform mTarget;
+	public float appearRange;
+	
+	private Animator mAnimator;
+	
+	private float mInvincibleTimer = 0.0f;
+	private float kInvincibilityDuration = 0.1f;
+
+	AudioSource strongHit;
+	AudioSource normalHit;
+	float audioTimer = 0.0f;
+
+	public Stats mStats;
+	
+	UICanvas uiCanvas;
+	private Vector3 damagePositionOffset = new Vector3 (0, 0.7f, 0);
+	
+	public int expGiven = 100;
+	
+	private int staggerTimer = 0;
+
 	void Start ()
 	{
-	
+		mStats = new Stats (1, 70, 18, 2, 0, new int[] { 20, 4, 2, 0 });
+		mAnimator = GetComponent<Animator> ();
+
+		mDying = false;
+
+		AudioSource[] audioSources = GetComponents<AudioSource> ();
+		normalHit = audioSources [0];
+		strongHit = audioSources [1];
+
+		uiCanvas = (UICanvas)GameObject.FindGameObjectWithTag ("UICanvas").GetComponent<UICanvas> ();
 	}
 
 	void Update ()
 	{
+		if (mStats.isDead () && !mDying) {
+			Die ();
+		}
+
+		UpdateAnimator ();
+		
+		if (mDying) {
+			dyingTimer += Time.deltaTime;
+			if (dyingTimer >= 0.8f) {
+				Destroy (gameObject);
+			}
+		}
+		
+		if (mGetHit) {
+			mInvincibleTimer += Time.deltaTime;
+			if (mInvincibleTimer >= kInvincibilityDuration) {
+				mGetHit = false;
+				mInvincibleTimer = 0.0f;
+			}
+		}
+		audioTimer += Time.deltaTime;
+	}
+
+	public void GetHit (Vector2 direction, int damage, bool isCrit)
+	{
+		Debug.Log ("Allo");
+
+		if (!mGetHit && !mDying) {
+			mGetHit = true;
+			
+			if (GameObject.Find ("Player").GetComponent<Player> ().IsStrongAttack ()) {
+				staggerTimer = 25 - (int)(staggerTimer * 0.10f);
+				damage = (int)(damage * 1.4f);
+				if (!strongHit.isPlaying)
+					strongHit.Play ();
+			} else {
+				staggerTimer = 15 - (int)(staggerTimer * 0.10f);
+				if (audioTimer >= 0.2f) {
+					normalHit.Play ();
+					audioTimer = 0.0f;
+				}
+			}
+			mStats.TakeDamage (damage);
+			if (isCrit) {
+				uiCanvas.CreateDamageLabel (((int)mStats.DamageDealt (damage)).ToString (), (transform.position + damagePositionOffset), UINotification.TYPE.CRIT);
+			} else {
+				uiCanvas.CreateDamageLabel (((int)mStats.DamageDealt (damage)).ToString (), (transform.position + damagePositionOffset), UINotification.TYPE.HPLOSS);
+			}
+		}
+	}
+
+	private void Die ()
+	{
+		mDying = true;
+	}
+
+	private void UpdateAnimator ()
+	{
+		mAnimator.SetBool ("isDying", mDying);
 	}
 }
--- Assets/Scripts/Player/PlayerFightCollider.cs
+++ Assets/Scripts/Player/PlayerFightCollider.cs
@@ -6,19 +6,23 @@ public class PlayerFightCollider : MonoBehaviour
 	void OnTriggerEnter (Collider col)
 	{
 		if (col.gameObject.name == "GeneralColliderHobo") {
-            col.gameObject.transform.parent.GetComponent<Hobo>().GetHit(gameObject.transform.parent.GetComponent<Player>().GetFacingDirection(), gameObject.transform.parent.GetComponent<Player>().mStats.DoDynamicDamage(), gameObject.transform.parent.GetComponent<Player>().mStats.wasCrit);
-            if (col.gameObject.transform.parent.GetComponent<Hobo>().mStats.isDead() && col.gameObject.transform.parent.GetComponent<Hobo>().expGiven != 0)
-            {
-                gameObject.transform.parent.GetComponent<Player>().AddExperience(col.gameObject.transform.parent.GetComponent<Hobo>().expGiven);
-                col.gameObject.transform.parent.GetComponent<Hobo>().expGiven = 0;
-            }
+			col.gameObject.transform.parent.GetComponent<Hobo> ().GetHit (gameObject.transform.parent.GetComponent<Player> ().GetFacingDirection (), gameObject.transform.parent.GetComponent<Player> ().mStats.DoDynamicDamage (), gameObject.transform.parent.GetComponent<Player> ().mStats.wasCrit);
+			if (col.gameObject.transform.parent.GetComponent<Hobo> ().mStats.isDead () && col.gameObject.transform.parent.GetComponent<Hobo> ().expGiven != 0) {
+				gameObject.transform.parent.GetComponent<Player> ().AddExperience (col.gameObject.transform.parent.GetComponent<Hobo> ().expGiven);
+				col.gameObject.transform.parent.GetComponent<Hobo> ().expGiven = 0;
+			}
 		} else if (col.gameObject.name == "GeneralColliderNeanderthal") {
-            col.gameObject.transform.parent.GetComponent<Neanderthal>().GetHit(gameObject.transform.parent.GetComponent<Player>().GetFacingDirection(), gameObject.transform.parent.GetComponent<Player>().mStats.DoDynamicDamage(), gameObject.transform.parent.GetComponent<Player>().mStats.wasCrit);
-            if (col.gameObject.transform.parent.GetComponent<Neanderthal>().mStats.isDead() && col.gameObject.transform.parent.GetComponent<Neanderthal>().expGiven != 0)
-            {
-                gameObject.transform.parent.GetComponent<Player>().AddExperience(col.gameObject.transform.parent.GetComponent<Neanderthal>().expGiven);
-                col.gameObject.transform.parent.GetComponent<Neanderthal>().expGiven = 0;
-            }
-        }
+			col.gameObject.transform.parent.GetComponent<Neanderthal> ().GetHit (gameObject.transform.parent.GetComponent<Player> ().GetFacingDirection (), gameObject.transform.parent.GetComponent<Player> ().mStats.DoDynamicDamage (), gameObject.transform.parent.GetComponent<Player> ().mStats.wasCrit);
+			if (col.gameObject.transform.parent.GetComponent<Neanderthal> ().mStats.isDead () && col.gameObject.transform.parent.GetComponent<Neanderthal> ().expGiven != 0) {
+				gameObject.transform.parent.GetComponent<Player> ().AddExperience (col.gameObject.transform.parent.GetComponent<Neanderthal> ().expGiven);
+				col.gameObject.transform.parent.GetComponent<Neanderthal> ().expGiven = 0;
+			}
+		} else if (col.gameObject.name == "EnemySpawner") {
+			col.gameObject.GetComponent<Spawner> ().GetHit (gameObject.transform.parent.GetComponent<Player> ().GetFacingDirection (), gameObject.transform.parent.GetComponent<Player> ().mStats.DoDynamicDamage (), gameObject.transform.parent.GetComponent<Player> ().mStats.wasCrit);
+			if (col.gameObject.GetComponent<Spawner> ().mStats.isDead () && col.gameObject.transform.parent.GetComponent<Neanderthal> ().expGiven != 0) {
+				gameObject.transform.parent.GetComponent<Player> ().AddExperience (col.gameObject.transform.parent.GetComponent<Neanderthal> ().expGiven);
+				col.gameObject.GetComponent<Spawner> ().expGiven = 0;
+			}
+		}
 	}
 }

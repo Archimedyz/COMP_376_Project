2e1cd133ce53aa5ac290f28e66390f3311a0f64a
Awais Ali <awais1994@hotmail.com>
2015-11-29 18:34:19 -0500

    Added visual warning for the metros

--- Assets/Scripts/Environment/MetroSpawner.cs
+++ Assets/Scripts/Environment/MetroSpawner.cs
@@ -13,14 +13,17 @@ public class MetroSpawner : MonoBehaviour {
     public float topOfTrack;
     public float bottomOfTrack;
 
-    GameObject mMainCameraRef; // for displaying the warnings.
+    GameObject mUICanvasRef; // for displaying the warnings.
+    [SerializeField]
+    GameObject mWarningBarPrefab;
+    GameObject mWarningBarRef;
 
     float spawnTimer;
 
 
 	// Use this for initialization
 	void Start () {
-        mMainCameraRef = GameObject.FindGameObjectWithTag("MainCamera");
+        mUICanvasRef = GameObject.FindGameObjectWithTag("UICanvas");
 	}
 	
 	// Update is called once per frame
@@ -34,8 +37,18 @@ public class MetroSpawner : MonoBehaviour {
             spawnedMetro.SendMessage("SetDistanceToLive", distanceToLive);
             spawnedMetro.SendMessage("SetTopOfTrack", topOfTrack);
             spawnedMetro.SendMessage("SetBottomOfTrack", bottomOfTrack);
-        } else if(spawnTimer >= (1.0f / spawnRate) + 1.5f) { // show some warning. sound the choo-choo.
-
+            Destroy(mWarningBarRef);
+            mWarningBarRef = null;
+        } else if(spawnTimer >= (1.0f / spawnRate) - 2.0f) { // show some warning. sound the choo-choo.
+            if(mWarningBarRef == null) {
+                mWarningBarRef = Instantiate(mWarningBarPrefab, mUICanvasRef.transform.position, Quaternion.identity) as GameObject;
+                mWarningBarRef.transform.SetParent(mUICanvasRef.transform, false);
+                mWarningBarRef.transform.localPosition = new Vector3((spawnRight ? -0.5f : 0.5f) * mUICanvasRef.GetComponent<RectTransform>().rect.width, 0.0f, 0.0f);
+                mWarningBarRef.transform.localScale = mWarningBarPrefab.transform.localScale;
+            } else {
+                mWarningBarRef.GetComponent<UnityEngine.UI.Image>().color = new Color(1.0f, 0.0f, 0.0f, (Mathf.Sin(Time.time * 5.0f) + 1) * 0.25f);
+                Debug.Log("Alpha" + (Mathf.Sin(Time.time) * 127.0f));
+            }
         }
 	}
 }

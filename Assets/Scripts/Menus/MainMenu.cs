acc0d204faeab2c27c589ed57b9f61bf0ec59ab1
Awais Ali <awais1994@hotmail.com>
2015-11-30 13:35:10 -0500

    Added Deletion of file to Main Menu.

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -17,7 +17,19 @@ public class MainMenu : MonoBehaviour
     private int mainMenuIndex = -1;
     private int newMenuIndex = -1;
     private int loadMenuIndex = -1;
+    private int deleteMenuIndex = -1;
 
+
+    private const int default_stage = 1;
+    private const int default_at_boss = 0;
+    private const int default_lvl = 1;
+    private const float default_max_hp = 60;
+    private const float default_hp = 60;
+    private const int default_str = 10;
+    private const int default_def = 5;
+    private const int default_spd = 5;
+
+    private int fileToDelete;
 
     // Use this for initialization
     void Start()
@@ -27,15 +39,11 @@ public class MainMenu : MonoBehaviour
         mainMenuPanels = GameObject.FindGameObjectsWithTag("MenuPanel");
         mainMenuButtons = new UnityEngine.UI.Button[mainMenuPanels.Length][];
 
+        PlayerPrefs.SetInt("game_loaded", 0);
+
         //* REMOVE
-        for (int i = 3; i < 4; ++i )
-        {
-            PlayerPrefs.SetInt("f" + i + "_lvl", 2);
-            PlayerPrefs.SetInt("f" + i + "_str", 12);
-            PlayerPrefs.SetInt("f" + i + "_def", 6);
-            PlayerPrefs.SetInt("f" + i + "_spd", 6);
-        }
-        // */
+        
+        //*/
 
         for (int i = 0; i < mainMenuPanels.Length; ++i)
         {
@@ -58,6 +66,8 @@ public class MainMenu : MonoBehaviour
                     loadButtonPanels[j] = mainMenuButtons[i][j].gameObject.GetComponentsInChildren<RectTransform>()[2].gameObject;
                     initLoadData(loadButtonPanels[j], j + 1);
                 }
+            } else if (mainMenuPanels[i].name == "DeletePanel") {
+                deleteMenuIndex = i;
             }
 
             if(i != mainMenuIndex) {
@@ -97,6 +107,10 @@ public class MainMenu : MonoBehaviour
         activeMenu = loadMenuIndex;
         mainMenuPanels[activeMenu].SetActive(true);
         mainMenuPanels[mainMenuIndex].SetActive(false);
+        // update all buttons in case they've changed
+        for(int i = 0; i < 3; ++i) { 
+            initLoadData(loadButtonPanels[i], i + 1);
+        }
     }
 
     void NewGame()
@@ -105,7 +119,6 @@ public class MainMenu : MonoBehaviour
         activeMenu = newMenuIndex;
         mainMenuPanels[activeMenu].SetActive(true);
         mainMenuPanels[mainMenuIndex].SetActive(false);
-
     }
 
     void Quit()
@@ -124,7 +137,14 @@ public class MainMenu : MonoBehaviour
 
     void NewFile1()
     {
-        Debug.Log("New Game - File 1");
+        if(PlayerPrefs.GetInt("f1_saved") == 1) {
+            // ask to overwrite the save file.
+            ShowDeleteFileDialogue(1);
+        } else {
+            // start a new game with the defaults.
+            InitNewFile(1);
+            LoadFile1();
+        }
     }
 
     void NewFile2()
@@ -135,6 +155,58 @@ public class MainMenu : MonoBehaviour
     void NewFile3()
     {
         Debug.Log("New Game - File 3");
+    }
+
+    void InitNewFile(int file_num)
+    {
+        PlayerPrefs.SetInt("files_saved", PlayerPrefs.GetInt("files_saved") + 1);
+        PlayerPrefs.SetInt("f" + file_num + "_saved", 1);
+        PlayerPrefs.SetInt("f" + file_num + "_stage", default_stage);
+        PlayerPrefs.SetInt("f" + file_num + "_at_boss", default_at_boss);
+        PlayerPrefs.SetInt("f" + file_num + "_lvl", default_lvl);
+        PlayerPrefs.SetFloat("f" + file_num + "_max_hp", default_max_hp);
+        PlayerPrefs.SetFloat("f" + file_num + "_hp", default_hp);
+        PlayerPrefs.SetInt("f" + file_num + "_str", default_str);
+        PlayerPrefs.SetInt("f" + file_num + "_def", default_def);
+        PlayerPrefs.SetInt("f" + file_num + "_spd", default_spd);
+    }
+
+    
+    void ShowDeleteFileDialogue(int fileToDelete)
+    {
+        this.fileToDelete = fileToDelete;
+        activeMenu = deleteMenuIndex;
+        buttonSelected = 1;
+        mainMenuPanels[deleteMenuIndex].SetActive(true);
+    }
+
+    void DeleteConfirm()
+    {
+        DeleteFile(fileToDelete);
+        initLoadData(newButtonPanels[fileToDelete-1], fileToDelete);
+        DeleteCancel();
+    }
+    
+    void DeleteCancel()
+    {
+        fileToDelete = 0;
+        activeMenu = newMenuIndex;
+        buttonSelected = 0;
+        mainMenuPanels[deleteMenuIndex].SetActive(false);
+    }
+
+    void DeleteFile(int file_num)
+    {
+        PlayerPrefs.SetInt("files_saved", PlayerPrefs.GetInt("files_saved") - 1);
+        PlayerPrefs.SetInt("f" + file_num + "_saved", 0);
+        PlayerPrefs.SetInt("f" + file_num + "_stage", 0);
+        PlayerPrefs.SetInt("f" + file_num + "_at_boss", 0);
+        PlayerPrefs.SetInt("f" + file_num + "_lvl", 0);
+        PlayerPrefs.SetFloat("f" + file_num + "_max_hp", 0.0f);
+        PlayerPrefs.SetFloat("f" + file_num + "_hp", 0.0f);
+        PlayerPrefs.SetInt("f" + file_num + "_str", 0);
+        PlayerPrefs.SetInt("f" + file_num + "_def", 0);
+        PlayerPrefs.SetInt("f" + file_num + "_spd", 0);
     }
 
     void LoadFile1()

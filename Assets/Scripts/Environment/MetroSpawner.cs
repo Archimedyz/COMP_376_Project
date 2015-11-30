0b262066dd2c67f7557dfc496c4dc66a9273766f
Awais Ali <awais1994@hotmail.com>
2015-11-29 19:05:48 -0500

    Finished Work on the metro spawner. Now spawns more trains when on tracks, and also plays annoying train sound when on tracks.

--- Assets/Scripts/Environment/MetroSpawner.cs
+++ Assets/Scripts/Environment/MetroSpawner.cs
@@ -7,15 +7,17 @@ public class MetroSpawner : MonoBehaviour {
     GameObject mMetroPrefabRef;
 
     public int spawnOrderInLayer;
-    public float spawnRate; // trains per second
     public bool spawnRight;
     public float distanceToLive;
     public float topOfTrack;
     public float bottomOfTrack;
 
     GameObject mUICanvasRef; // for displaying the warnings.
+    Player mPlayerRef; // to adjust the spawnrates.
+
     [SerializeField]
     GameObject mWarningBarPrefab;
+    AudioSource mTrainWarningAudio;
     GameObject mWarningBarRef;
 
     float spawnTimer;
@@ -24,12 +26,15 @@ public class MetroSpawner : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         mUICanvasRef = GameObject.FindGameObjectWithTag("UICanvas");
+        mPlayerRef = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        mTrainWarningAudio = GetComponent<AudioSource>();
+        ResetSpawnTimer();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        spawnTimer += Time.deltaTime;
-	    if(spawnTimer >= (1.0f / spawnRate)) {
+        spawnTimer -= Time.deltaTime;
+	    if(spawnTimer <= 0.0f) {
             spawnTimer = 0.0f;
             GameObject spawnedMetro = Instantiate(mMetroPrefabRef, transform.position, Quaternion.identity) as GameObject;
             spawnedMetro.SendMessage("SetOrderInLayer", spawnOrderInLayer);
@@ -39,16 +44,24 @@ public class MetroSpawner : MonoBehaviour {
             spawnedMetro.SendMessage("SetBottomOfTrack", bottomOfTrack);
             Destroy(mWarningBarRef);
             mWarningBarRef = null;
-        } else if(spawnTimer >= (1.0f / spawnRate) - 2.0f) { // show some warning. sound the choo-choo.
+            ResetSpawnTimer();
+        } else if(spawnTimer <= 2.5f) { // show some warning. sound the choo-choo.
             if(mWarningBarRef == null) {
                 mWarningBarRef = Instantiate(mWarningBarPrefab, mUICanvasRef.transform.position, Quaternion.identity) as GameObject;
                 mWarningBarRef.transform.SetParent(mUICanvasRef.transform, false);
                 mWarningBarRef.transform.localPosition = new Vector3((spawnRight ? -0.5f : 0.5f) * mUICanvasRef.GetComponent<RectTransform>().rect.width, 0.0f, 0.0f);
                 mWarningBarRef.transform.localScale = mWarningBarPrefab.transform.localScale;
+                if(mPlayerRef.mFloorIndex == 1) {
+                    mTrainWarningAudio.Play();
+                }
             } else {
                 mWarningBarRef.GetComponent<UnityEngine.UI.Image>().color = new Color(1.0f, 0.0f, 0.0f, (Mathf.Sin(Time.time * 5.0f) + 1) * 0.25f);
-                Debug.Log("Alpha" + (Mathf.Sin(Time.time) * 127.0f));
             }
         }
 	}
+
+    void ResetSpawnTimer() {
+        float spawnBaseCooldown = (mPlayerRef.mFloorIndex == 1) ? 7.5f : 15.0f;
+        spawnTimer = spawnBaseCooldown - Random.Range(0.0f, spawnBaseCooldown * 0.2f); // allow for fluctuation between spawn times
+    }
 }

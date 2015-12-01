dfaa68f24eeab9f534491cd644ecf48e3864cc60
Awais Ali <awais1994@hotmail.com>
2015-12-01 00:05:14 -0500

    Fixed issues with the background, should now always line up properly for any scene with a floor.

--- Assets/Scripts/Environment/ParallaxBackground.cs
+++ Assets/Scripts/Environment/ParallaxBackground.cs
@@ -6,6 +6,7 @@ public class ParallaxBackground : MonoBehaviour {
     private ParallaxLayer[] mLayers;
     private Vector3 mInitPosition;
     private float oldX;
+    private float absoluteY = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,14 +14,18 @@ public class ParallaxBackground : MonoBehaviour {
         for (int i = 0; i < mLayers.Length; ++i)
         {
             mLayers[i].SetParallaxFactor(0.10f - 0.025f * i);
+            absoluteY = Mathf.Max(mLayers[i].GetComponent<SpriteRenderer>().bounds.size.y / 2.0f, absoluteY);
         }
         mInitPosition = transform.position;
         oldX = transform.position.x;
+        GameObject firstFloor = FindObjectOfType<FloorController>().GetComponentsInChildren<Floor>()[0].gameObject;
+        Debug.Log(firstFloor.name + ": " + absoluteY + " + " + firstFloor.transform.position.y + " + " + (firstFloor.GetComponentInChildren<SpriteRenderer>().bounds.size.y / 2.0f));
+        absoluteY += firstFloor.transform.position.y + (firstFloor.GetComponentInChildren<SpriteRenderer>().bounds.size.y / 2.0f);  
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector3(transform.position.x, mInitPosition.y, mInitPosition.z);
+        transform.position = new Vector3(transform.position.x, absoluteY, mInitPosition.z);
         if(transform.position.x != oldX) {
             float deltaX = transform.position.x - oldX;
             oldX = transform.position.x;

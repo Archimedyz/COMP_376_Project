6df3f213a8a148e501aeee740c3501dce2d4a9ac
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2015-12-03 22:44:12 -0500

    Redirect coconut
    ....

--- Assets/Scripts/Enemies/Neanderthal/Coconut.cs
+++ Assets/Scripts/Enemies/Neanderthal/Coconut.cs
@@ -20,6 +20,11 @@ public class Coconut : MonoBehaviour
 
 	private Transform player;
 
+	private float initializedTime;  // when is the coconut instantiated
+	private float timeSincePlayerStartedDefending; // did they defend right after coconut was thrown
+	private Rigidbody CoconutRGBD;
+	private bool redirected;
+
 	void Start ()
 	{
 		player = GameObject.Find ("Player").transform;
@@ -27,21 +32,30 @@ public class Coconut : MonoBehaviour
 		canThrow = true;
 		throwed = false;
 		Timer = 0.0f;
+
+		initializedTime = Time.timeSinceLevelLoad; // the exact time the cocunut was created
+		CoconutRGBD = GetComponent<Rigidbody>();
+		redirected = false;
 	}
 
 	void Update ()
 	{
 		Timer += Time.deltaTime;
 
-		if (Timer >= 0.24f && Timer <= 0.3f) {
-			transform.position = new Vector3 (transform.parent.position.x - 0.1f, transform.parent.position.y + 0.5f, transform.position.z);
-		} else if (Timer >= 0.12f && Timer < 0.24f) {
-			transform.position = new Vector3 (transform.parent.position.x - 0.5f, transform.parent.position.y - 0.1f, transform.position.z);
-		} else if (Timer >= 0.3f) {
-			if (canThrow)
-				Throw ();
-			transform.Translate (Vector3.Normalize (player.position - transform.position) * mThrowForce * Time.deltaTime);
-		}
+		// capture when player started defending
+		if(Input.GetKeyDown(KeyCode.N) || Input.GetKeyDown(KeyCode.V))
+			timeSincePlayerStartedDefending = Time.timeSinceLevelLoad;
+
+		if(!redirected)
+			if (Timer >= 0.24f && Timer <= 0.3f) {
+				transform.position = new Vector3 (transform.parent.position.x - 0.1f, transform.parent.position.y + 0.5f, transform.position.z);
+			} else if (Timer >= 0.12f && Timer < 0.24f) {
+				transform.position = new Vector3 (transform.parent.position.x - 0.5f, transform.parent.position.y - 0.1f, transform.position.z);
+			} else if (Timer >= 0.3f) {
+				if (canThrow)
+					Throw ();
+				transform.Translate (Vector3.Normalize (player.position - transform.position) * mThrowForce * Time.deltaTime);
+			}
 
 		if (throwed) {
 			if (transform.position.x - initialPositionX >= maxDisplacement || Timer >= maxExistTime) {
@@ -70,13 +84,53 @@ public class Coconut : MonoBehaviour
 
 	void OnCollisionEnter (Collision col)
 	{
-		if (col.gameObject.name == "Player") {
-			if (col.gameObject.GetComponent<Player> ().IsDefending ()) {
-				col.gameObject.GetComponent<Player> ().GetBlockDamage (damage / 3);
-			} else
-				col.gameObject.GetComponent<Player> ().GetKnockdown (mDirection, damage);
+		if (col.gameObject.name == "Player")   
+		{
+			if (timeSincePlayerStartedDefending > initializedTime && player.position.x > transform.position.x 
+			    						&& player.gameObject.GetComponent<Player>().GetFacingDirection() == Vector2.left)    // redirect it back to enemy
+			{
+				redirected = true;
+				CoconutRGBD.AddForce (10 * Vector3.left, ForceMode.VelocityChange);
+			}
+			else if (timeSincePlayerStartedDefending > initializedTime && player.position.x < transform.position.x 
+			         					&& player.gameObject.GetComponent<Player>().GetFacingDirection() == Vector2.right)    // redirect it back to enemy
+			{
+				redirected = true;
+				CoconutRGBD.AddForce (10 * Vector3.right, ForceMode.VelocityChange);
+				
+			}else
+			{
+				if (col.gameObject.GetComponent<Player> ().IsDefending ()) {
+					col.gameObject.GetComponent<Player> ().GetBlockDamage (damage / 3);
+				} else
+					col.gameObject.GetComponent<Player> ().GetKnockdown (mDirection, damage);
 
+				Destroy (gameObject);
+			}
+		}
+
+		// if coconut hits another enemy do damage to them
+		if (col.gameObject.name == "Hobo") {
+			col.gameObject.GetComponent<Hobo>().GetHit(GetDirection(), 5, false); 
 			Destroy (gameObject);
+		}
+		if (col.gameObject.name == "Neanderthal") {
+			if(Time.timeSinceLevelLoad > initializedTime + 0.5f)
+			{
+				col.gameObject.GetComponent<Neanderthal>().GetHit(GetDirection(), 5, false); 
+				Destroy(gameObject);
+			}
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
